9ecc9b2ab3c7971301fbc2608993caf84a8ae300
n4y0n <alfredomontada0@gmail.com>
2018-04-12 16:12:48 +0200

    Updated .gitignore

--- dist/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var XBee_1 = require("./src/XBee");
-var xbee1 = new XBee_1.XBee('COM1');
-xbee1.on("data", function (data) {
-    console.log(data);
-    xbee1.sendData(data);
-});
